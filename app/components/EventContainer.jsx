6e250d9c7a750bb10219844fb76afb95566466e4
Trevor Sanderson <tjsande@umich.edu>
2017-11-16 18:51:38 -0500

    initial commit

---
+++ app/components/EventContainer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+import Rectangle from 'react-rectangle';
+
+
+const EventContainer = ({headerText, subText}) => (
+	<Box>
+		<Header> this.headerText
+			<subHead>this.subText
+			</subHead>
+		</Header>
+	</Box>
+
+);
+
+render() {
+	return (
+		
+	);
+}
