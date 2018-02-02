0045a19c9ce37bd637de289076a6fea48994e5f5
Tarun Khubchandani <tarunsk@umich.edu>
2018-02-01 20:45:03 -0500

    Section text components (#11)
    * Edited font family
    
    * Added components
    
    * Cleaning up using prettier
    
    * Fixes PR comments
    
    * Pleasing prettier
    
    * Edited font family
    
    * Added components
    
    * Cleaning up using prettier
    
    * Fixes PR comments
    
    * Pleasing prettier
    
    * Cleanup page

--- app/pages/home/section_components.jsx
+++ app/pages/home/section_components.jsx
@@ -22,22 +22,41 @@ const CenteredContainer = styled.div`
 `;
 
 const SectionHeader = styled.h2`
-    fontSize: 42px;
-    color: ${props => props.theme.highlight};
+    fontSize: 48px;
+    letterSpacing: 5px;
+    color: ${props => (props.dark ? 'white' : props.theme.secondary)};
     textTransform: uppercase;
-    text-align: center;
-    fontWeight: 500;
+    fontWeight: bold;
     textAlign: center;
 
-    ${devices.small`
-    fontSize: 48px;
+    ${devices.tablet`
+    fontSize: 72px;
+    letterSpacing: 10px;
+    `};
+`;
+
+const SectionSubheader = styled.h3`
+    fontSize: 24px;
+    letterSpacing: 2px;
+    color: ${props => (props.dark ? 'white' : props.theme.secondary)};
+    fontWeight: book;
+    textAlign: left;
+
+    ${devices.tablet`
+    fontSize: 36px;
+    letterSpacing: 5px;
     `};
 `;
 
 const SectionBody = styled.p`
-    color: white;
     fontSize: 16px;
-    max-width: 600px;
+    color: ${props => (props.dark ? 'white' : props.theme.secondary)};
+    fontWeight: book;
+    textAlign: left;
+
+    ${devices.tablet`
+    fontSize: 24px;
+    `};
 `;
 
 const Brick = styled.div`
@@ -46,4 +65,10 @@ const Brick = styled.div`
     background: ${props => props.theme.highlight};
 `;
 
-export { CenteredContainer, SectionHeader, SectionBody, Brick };
+export {
+    CenteredContainer,
+    SectionHeader,
+    SectionSubheader,
+    SectionBody,
+    Brick
+};
